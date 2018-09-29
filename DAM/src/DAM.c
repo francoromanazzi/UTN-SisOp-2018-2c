6ff6790c24dfb3a68a46a9901ecab74a02d36d0a
rodillos <rodillob9@gmail.com>
2018-09-29 17:26:47 -0300

    creadas las funcionalidades para enviar y recibir archivos del MDJ al FM9 pasando por el DAM, falta revisarlo2

--- DAM/src/DAM.c
+++ DAM/src/DAM.c
@@ -1,4 +1,4 @@
-#include "DAM.h"
+1#include "DAM.h"
 
 int main(void) {
 	if(dam_initialize() == -1){
@@ -97,14 +97,14 @@ void dam_nuevo_cliente_iniciar(int socket){
 		t_msg* nuevo_mensaje = malloc(sizeof(t_msg));
 		if(msg_await(socket, nuevo_mensaje) == -1){
 			log_info(logger, "Cierro el hilo que atendia a este cliente");
-			// TODO: Cerrar el hilo?
 			msg_free(&nuevo_mensaje);
+			pthread_exit();
 			return;
 		}
 		if(dam_manejar_nuevo_mensaje(socket, nuevo_mensaje) == -1){
 			log_info(logger, "Cierro el hilo que atendia a este cliente");
-			// TODO: Cerrar el hilo?
 			msg_free(&nuevo_mensaje);
+			pthread_exit();
 			return;
 		}
 		msg_free(&nuevo_mensaje);
@@ -114,7 +114,7 @@ void dam_nuevo_cliente_iniciar(int socket){
 int dam_manejar_nuevo_mensaje(int socket, t_msg* msg){
 	log_info(logger, "EVENTO: Emisor: %d, Tipo: %d, Tamanio: %d, Mensaje: %s",msg->header->emisor,msg->header->tipo_mensaje,msg->header->payload_size,(char*) msg->payload);
 
-	int ok;
+	int ok,resultadoManejar;
 	unsigned int id;
 	char* path;
 	int base;
@@ -127,33 +127,94 @@ int dam_manejar_nuevo_mensaje(int socket, t_msg* msg){
 
 			case DESCONEXION:
 				log_info(logger, "Se desconecto CPU");
-				return -1;
+				resultadoManejar = -1;
 			break;
 
 			case ABRIR:
 				desempaquetar_abrir(msg,&path,&id);
-				log_info(logger, "Ehhh, voy a buscar %s para %d", path, id);
+				log_info(logger, "Ehhh, voy a busar %s para %d", path, id);
 
-				// TODO: preguntar a MDJ, recibir de MDJ, escribir en FM9, recibir resultado de FM9
+				// TODO: solicitud de archivo desde MDJ
+				//que pasa si no lo tiene?
+				t_msg* mensajeAbrir = msg_create(DAM,ABRIR,(void**) path, strlen(path));
+				if((msg_send(mdj_socket, mensajeAbrir)) == -1){
+					log_info(logger, "Problema con solicitud de archivo");
+					msg_free(&mensajeAbrir);
+					resultadoManejar = -1;
+					break;
+				}
+
+				resultadoManejar = 1;
+				break;
+
+
+				//TODO: recibir datos de FM9 para G.DT
 
 				/* Le mando a SAFA el resultado de abrir el archivo */
 				/* HARDCODEO*/ ok = 1; base = 21198;
 				dam_send(safa_socket, RESULTADO_ABRIR, ok, id, path, base);
 				free(path);
+				resultadoManejar = 1;
 			break;
 
 			case FLUSH:
 				log_info(logger, "Iniciando operacion FLUSH");
+
+				//si es OK
+				resultadoManejar = 1;
 			break;
 
 			default:
 				log_info(logger, "No entendi el mensaje de CPU");
+				//no deberia ser 1, sino otro numero, para identificar el "ERROR"
+				resultadoManejar = 1;
+
 		}
+	}
+	else if(msg->header->emisor == MDJ){
+				//TODO: recibir archivo de MDJ
+				//TODO: cargar en FM9 el archivo
+				//hace la recepción del resto del archivo desde MDJ y envia To.do el archivo a FM9
+		t_msg* avisoApertura = msg_create(DAM,ABRIR,path, strlen(path));
+		if (msg_send(SAFA, avisoApertura) != -1){
+			resultadoManejar = enviarArchivo((void**) msg->payload, msg->header->payload_size);
+		}else{
+			resultadoManejar = -1;
+		}
+
 	}
 	else if(msg->header->emisor == DESCONOCIDO){
 		log_info(logger, "Me hablo alguien desconocido");
+		//si esto es OK
+		resultadoManejar = 1;
 	}
-	return 1;
+
+	return resultadoManejar;
+}
+
+int enviarArchivo(void** payload, int tamPayload){
+	int resultadoTransferencia = 1;
+
+	if(tamPayload != -1){
+
+		t_msg* mensajeResultado = msg_create(DAM,RESULTADO_ABRIR,payload, tamPayload);
+
+		if(msg_send(fm9_socket, mensajeResultado) == -1){
+			log_info(logger, "Problemas al enviar linea de archivo a FM9");
+			msg_free(mensajeResultado);
+			resultadoTransferencia = -1;
+		}
+
+		if (msg_await(MDJ,mensajeResultado) == -1){
+			log_info(logger, "Problema al recibir linea de archivo de MDJ");
+			resultadoTransferencia = -1;
+		}else{
+			resultadoTransferencia = enviarArchivo(mensajeResultado->payload, mensajeResultado->header->payload_size);
+		}
+
+		msg_free(mensajeResultado);
+	}
+	return resultadoTransferencia;
 }
 
 int dam_connect_to_safa(){
